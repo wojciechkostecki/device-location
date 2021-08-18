c66551b2166fc49417abf9b74e4867154b46a108
48531 <wt.kostecki@gmail.com>
2021-08-18 10:11:31 +0200

    Initial commit

---
+++ src/test/java/pl/wojciechkostecki/devicelocation/DeviceLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wojciechkostecki.devicelocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceLocationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
