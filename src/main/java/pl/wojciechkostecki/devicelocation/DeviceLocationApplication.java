f3ddeda5a74af32fbc118d023214d3ceac8c1b62
48531 <wt.kostecki@gmail.com>
2021-08-18 21:03:25 +0200

    Adding the functionality to save Roles and Administrator after starting the application.

--- src/main/java/pl/wojciechkostecki/devicelocation/DeviceLocationApplication.java
+++ src/main/java/pl/wojciechkostecki/devicelocation/DeviceLocationApplication.java
@@ -2,12 +2,55 @@ package pl.wojciechkostecki.devicelocation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import pl.wojciechkostecki.devicelocation.model.Role;
+import pl.wojciechkostecki.devicelocation.model.User;
+import pl.wojciechkostecki.devicelocation.model.UserRole;
+import pl.wojciechkostecki.devicelocation.repository.UserRepository;
+import pl.wojciechkostecki.devicelocation.repository.UserRoleRepository;
 
 @SpringBootApplication
 public class DeviceLocationApplication {
+    private final UserRoleRepository userRoleRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public DeviceLocationApplication(UserRoleRepository userRoleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRoleRepository = userRoleRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DeviceLocationApplication.class, args);
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void afterInitSetup() {
+        saveRoles();
+        saveAdmin();
+    }
+
+    private void saveAdmin() {
+        if(!userRepository.findByUsername("admin").isPresent()) {
+            User user = new User();
+            user.setUsername("admin");
+            user.setPassword(passwordEncoder.encode("admin"));
+            user.getRoles().add(userRoleRepository.findByName(Role.ADMIN).get());
+            user.getRoles().add(userRoleRepository.findByName(Role.USER).get());
+            userRepository.save(user);
+        }
+    }
+
+    private void saveRoles() {
+        for (Role role : Role.values()) {
+            if(!userRoleRepository.existsByName(role)) {
+                UserRole userRole = new UserRole();
+                userRole.setName(role);
+                userRoleRepository.save(userRole);
+            }
+        }
+    }
 }
--- src/main/java/pl/wojciechkostecki/devicelocation/controller/AuthController.java
+++ src/main/java/pl/wojciechkostecki/devicelocation/controller/AuthController.java
@@ -14,8 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.wojciechkostecki.devicelocation.exception.LoginAlreadyUsedException;
 import pl.wojciechkostecki.devicelocation.mapper.UserMapper;
+import pl.wojciechkostecki.devicelocation.model.Role;
+import pl.wojciechkostecki.devicelocation.model.User;
 import pl.wojciechkostecki.devicelocation.model.dto.UserDTO;
 import pl.wojciechkostecki.devicelocation.repository.UserRepository;
+import pl.wojciechkostecki.devicelocation.repository.UserRoleRepository;
 import pl.wojciechkostecki.devicelocation.security.TokenUtil;
 
 @RestController
@@ -26,13 +29,15 @@ public class AuthController {
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
     private final TokenUtil tokenUtil;
+    private final UserRoleRepository userRoleRepository;
 
-    public AuthController(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, TokenUtil tokenUtil) {
+    public AuthController(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, TokenUtil tokenUtil, UserRoleRepository userRoleRepository) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
         this.passwordEncoder = passwordEncoder;
         this.authenticationManager = authenticationManager;
         this.tokenUtil = tokenUtil;
+        this.userRoleRepository = userRoleRepository;
     }
 
     @PostMapping("/register")
@@ -41,7 +46,9 @@ public class AuthController {
             throw new LoginAlreadyUsedException("There is a user with given login");
         } else {
             userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
-            userRepository.save(userMapper.toEntity(userDTO));
+            User user = userMapper.toEntity(userDTO);
+            user.getRoles().add(userRoleRepository.findByName(Role.USER).get());
+            userRepository.save(user);
             return new ResponseEntity<>(HttpStatus.CREATED);
         }
     }
