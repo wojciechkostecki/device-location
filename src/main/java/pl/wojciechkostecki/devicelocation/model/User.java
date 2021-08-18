3ea694a45f6a3360d10864cf45e60ad7e2e3ae2c
48531 <wt.kostecki@gmail.com>
2021-08-18 19:11:21 +0200

    Creation of an entity and DTO for the User and creation of a Role.

---
+++ src/main/java/pl/wojciechkostecki/devicelocation/model/User.java
@@ -0,0 +1,29 @@
+package pl.wojciechkostecki.devicelocation.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.DETACH})
+    @JoinTable(name = "user_roles",
+            joinColumns = {@JoinColumn (name = "user_id")},
+            inverseJoinColumns = {@JoinColumn (name = "roles_id")})
+    private Set<UserRole> roles = new HashSet<>();
+
+}
---
+++ src/main/java/pl/wojciechkostecki/devicelocation/model/dto/UserDTO.java
@@ -0,0 +1,12 @@
+package pl.wojciechkostecki.devicelocation.model.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private Long id;
+
+    private String username;
+
+    private String password;
+}
