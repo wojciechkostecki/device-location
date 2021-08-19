7bb4f4cbf9ebee406394af23194f1709971b4a20
48531 <wt.kostecki@gmail.com>
2021-08-19 14:20:55 +0200

    Setting the log level and adding exceptions to global handler

--- src/main/java/pl/wojciechkostecki/devicelocation/controller/GlobalExceptionHandler.java
+++ src/main/java/pl/wojciechkostecki/devicelocation/controller/GlobalExceptionHandler.java
@@ -3,11 +3,14 @@ package pl.wojciechkostecki.devicelocation.controller;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import pl.wojciechkostecki.devicelocation.exception.LoginAlreadyUsedException;
+
+import javax.persistence.EntityNotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -16,6 +19,22 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             (LoginAlreadyUsedException exception, WebRequest request) {
         HttpHeaders headers = new HttpHeaders();
         HttpStatus status = HttpStatus.CONFLICT;
+        return handleExceptionInternal(exception, null, headers, status, request);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public final ResponseEntity<Object> handleEntityNotFoundException
+            (EntityNotFoundException exception, WebRequest request) {
+        HttpHeaders headers = new HttpHeaders();
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        return handleExceptionInternal(exception, null, headers, status, request);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public final ResponseEntity<Object> handleBadCredentialsException
+            (BadCredentialsException exception, WebRequest request) {
+        HttpHeaders headers = new HttpHeaders();
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
         return handleExceptionInternal(exception, null, headers, status, request);
     }
 }
